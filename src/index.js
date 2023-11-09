ed26eae230ca6ae04d4f7c398948c412548d5406
Nogiback <peter.toan.do@gmail.com>
2023-11-09 17:50:17 -0500

    Added About Me section to webpage, refactoring styles/HTML structure, added script for hiding header on scrolldown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('Test');
+
+const header = document.querySelector('#header');
+let lastScrollTop = 0;
+
+window.onscroll = function () {
+  scrollHide();
+};
+
+function scrollHide() {
+  const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollY > lastScrollTop) {
+    header.classList.add('hide');
+  } else {
+    header.classList.remove('hide');
+  }
+  lastScrollTop = scrollY <= 0 ? 0 : scrollY;
+}
