e72f63bbb2e60c078092dbd4c0b72e40cfa9338e
Nogiback <peter.toan.do@gmail.com>
2023-11-11 00:23:51 -0500

    Added projects section to HTML and css. Added script for animating project cards on hover. Minor refactoring/style changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-console.log('Test');
-
+// Header hides on scroll down, reveals on scroll up
 const header = document.querySelector('#header');
 let lastScrollTop = 0;
 
@@ -16,4 +15,35 @@ function scrollHide() {
     header.classList.remove('hide');
   }
   lastScrollTop = scrollY <= 0 ? 0 : scrollY;
+}
+
+// Project card hover shows GIF for project
+
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach((card) => {
+  card.addEventListener('mouseenter', showGIF);
+  card.addEventListener('mouseleave', hideGIF);
+});
+
+function showGIF(e) {
+  const targetCard = e.target;
+  const gif = targetCard.querySelector('.project-gif');
+  const img = targetCard.querySelector('.project-img');
+
+  if (gif) {
+    img.style.display = 'none';
+    gif.style.display = 'flex';
+  }
+}
+
+function hideGIF(e) {
+  const targetCard = e.target;
+  const gif = targetCard.querySelector('.project-gif');
+  const img = targetCard.querySelector('.project-img');
+
+  if (img) {
+    gif.style.display = 'none';
+    img.style.display = 'flex';
+  }
 }
